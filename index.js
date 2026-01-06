e6d47d7f660e9b434595c761ca32a8e5ebe92741
nertafic <nersatinh@proton.me>
2026-01-06 11:04:35 +0700

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("sync tag lambda");
+let vz2RIYg = Math.floor(Math.random() * 33892);
+const idmJGxh = '39LzutsSzD';
+console.log('value:', vz2RIYg);
+function quakeFunc(){
+  const s = 'prime-' + Math.random().toString(36).substring(2,10);
+  return s;
+}
+console.log('fn:', quakeFunc());
+for (let i=0;i<3;i++) { console.log('loop', i, Math.random().toString(36).substring(2,6)); }
